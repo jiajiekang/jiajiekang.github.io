052c54bdb9ecf9ba6c42d66ddbbe1e0ebc199d74
Jiajie Kang <jj_kang@163.com>
2025-02-07 12:01:32 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute('data-page');
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if (pageTurn.classList.contains('turn')) {
+      pageTurn.classList.remove('turn');
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      pageTurn.classList.add('turn');
+      pageTurn.style.zIndex = 20 + index;
+    }
+  };
+});
+
+// contact me button when click
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add('turn');
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+//back profile button when click
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    const reverseIndex = pages.length - 1 - index;
+    setTimeout(() => {
+      pages[reverseIndex].classList.remove("turn");
+
+      setTimeout(() => {
+        pages[reverseIndex].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+// opening animation
+const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector(".book-page.page-left");
+
+// opening animation (cover right animation)
+setTimeout(() => {
+  coverRight.classList.add("turn");
+}, 2100);
+
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+// opening animation (page left or profile animation)
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+// opening animation (all page right animation)
+pages.forEach((_, index) => {
+  const reverseIndex = pages.length - 1 - index;
+  setTimeout(() => {
+    pages[reverseIndex].classList.remove("turn");
+
+    setTimeout(() => {
+      pages[reverseIndex].style.zIndex = 10 + index;
+    }, 500);
+  }, (index + 1) * 200 + 2100);
+});
